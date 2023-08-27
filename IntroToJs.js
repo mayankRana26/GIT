375250b37035900e213cd6ab28c6ed9ac7c4f385
mayankRana26 <ranamayank987@gmail.com>
2023-08-27 08:39:23 +0530

    I have done it

---
+++ IntroToJs.js
@@ -0,0 +1,74 @@
+//Q1..... Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai School\n A Transformation in Education in next line")
+
+
+                    //Q2....
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+
+let Name= "Mayank Rana"
+console.log(Name);
+
+let fatherName="Mr.Rajendra singh Rana"
+console.log(fatherName);
+
+let motherName="late.Smt.Shakuntala devi"
+console.log(motherName)
+
+
+
+
+                //    Q3..
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+
+let nAme="Maynk"
+let age=21;
+console.log(nAme,age);
+console.log(typeof(nAme),typeof(age));
+
+
+
+
+
+
+                    // Q4....
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let studentName = "Rohit";
+let school = "ABC ";
+let grade = "12th";
+let section = "B";
+let rollNo = "249161";
+let subject1 = "Maths";
+let subject2 = "Science";
+let subject3 = "English";
+let marksSubject1 = 90;
+let marksSubject2 = 80;
+let marksSubject3 = 91;
+
+// Printing the report card
+console.log("**********************************************");
+console.log("            STUDENT REPORT CARD                ");
+console.log("**********************************************");
+console.log("Name: " + studentName);
+console.log("School: " + school);
+console.log("Grade: " + grade);
+console.log("Section: " + section);
+console.log("Roll No: " + rollNo);
+console.log("----------------------------------------------");
+console.log("                 Marks                     ")
+console.log("----------------------------------------------");
+console.log(subject1  + " " + marksSubject1);
+console.log(subject2 +" " + marksSubject2);
+console.log(subject3  +" " + marksSubject3);
+console.log("----------------------------------------------");
---
+++ mathOperators.js
@@ -0,0 +1,8 @@
+// Q1....Print Masai School==>
+console.log("Masai School")
+
+// Q2...  Multiply the Varaible number by 50,and print the result
+
+let num=2
+let result=num*50
+console.log(result)
